e92957ae162f3e29084bc63cb9fb2247ffccacff
hashimoto091 <s1542091ZA@s.chibakoudai.jp>
2017-06-30 15:05:32 +0900

    3.絞り込み

--- sample/shop/shop_list.php
+++ sample/shop/shop_list.php
@@ -3,98 +3,122 @@ session_start();
 session_regenerate_id(true);
 if(isset($_SESSION['member_login'])==false)
 {
-	print 'ようこそゲスト様　';
-	print '<a href="member_login.html">会員ログイン</a><br />';
-	print '<br />';
+ print 'ようこそゲスト様　';
+ print '<a href="member_login.html">会員ログイン</a><br />';
+ print '<br />';
 }
 else
 {
-	print 'ようこそ';
-	print $_SESSION['member_name'];
-	print ' 様　';
-	print '<a href="member_logout.php">ログアウト</a><br />';
-	print '<br />';
+ print 'ようこそ';
+ print $_SESSION['member_name'];
+ print ' 様　';
+ print '<a href="member_logout.php">ログアウト</a><br />';
+ print '<br />';
 }
 ?>
-
 <!DOCTYPE html>
 <html>
 <head>
 <meta charset="UTF-8">
-<title>YOMOTTO書籍</title>
+<title>まる自動車販売店</title>
 </head>
 <body>
-
 <?php
-
 try
 {
-
 $dsn='mysql:dbname=shop;host=localhost;charset=utf8';
 $user='root';
 $password='';
 $dbh=new PDO($dsn,$user,$password);
 $dbh->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
-
-$sql='SELECT code,name,price FROM mst_product WHERE 1';
+$sql='SELECT code,name,price,maker,price2,distance FROM mst_product WHERE 1';
 $stmt=$dbh->prepare($sql);
 $stmt->execute();
-
 $dbh=null;
-
 print '商品一覧<br /><br />';
-
+require_once('../common/common.php');
 ?>
-
 <form method="post" action="">
 キーワード<br />
-<input type="text" name="keyword" ><br />
-<br />
+<input type="text" name="keyword"><br />
 <input type="submit" value="検索">
+</form> 
+<br />
+キーワードを選んでください。<br />
+<form method="post"action="">
+メーカー
+<?php pulldown_type(); ?>
+価格
+<?php pulldown_price(); ?>
+走行距離
+<?php pulldown_distance(); ?>
+<br />
+<input type="submit" value="絞り込み">
 </form>
-
+<br />
 <?php
-
-$key='';
+//フリーキーワード
+$keyword='';
 if(isset($_POST['keyword'])){
-  $key=$_POST['keyword'];
+    $keyword=$_POST['keyword'];
 }
-if($key!==''){
-  print $key.'が含まれる商品';
-  print '<br/>';
+if($keyword!==''){
+    print $keyword.'が含まれる';
+    print '<br />';
 }
-
-
+//固定キーワード
+$maker='';
+$price='';
+$distance='';
+if(isset($_POST['maker'])){
+   $maker=$_POST['maker'];
+   $price=$_POST['price'];
+   $distance=$_POST['distance'];
+}
+if($maker!==''){
+    print $maker.' '.$price.','.$distance.'に一致する商品';
+    print '<br />';
+}
 while(true)
 {
-	$rec=$stmt->fetch(PDO::FETCH_ASSOC);
-	if($rec==false)
-	{
-		break;
-	}
-	
-	if(($key==='')||(strpos($rec['name'],$key)!==false)){
-	
-	print '<a href="shop_product.php?procode='.$rec['code'].'">';
-	print $rec['name'].'---';
-	print $rec['price'].'円';
-	print '</a>';
-	print '<br />';
-	
-	}
+ $rec=$stmt->fetch(PDO::FETCH_ASSOC);
+            $maker2=$rec['maker'];
+            $price2=$rec['price2'];
+            $distance2=$rec['distance'];
+ if($rec==false)
+ {
+  break;
+ }
+        
+            $disp=0;
+        //キーワードが空、または、キーワードが含まれるとき表示
+        if(($keyword==='')&&($maker==='')){
+            $disp=1;
+        }
+        else if(($maker==='')&&(strpos($rec['name'],$keyword)!==false)){
+            $disp=1;
+        }
+        else if(($keyword==='')&&((strpos($maker2,$maker)!==false)&&(strpos($price2,$price)!==false)&&(strpos($distance2,$distance)!==false))){
+            $disp=1;
+        }
+            
+    if($disp===1){
+ print '<a href="shop_product.php?procode='.$rec['code'].'">';
+ print $rec['name'].'---';
+ print $rec['price'].'円';
+ print '</a>';
+ print '<br />';
+    }
 }
-
+   
 print '<br />';
 print '<a href="shop_cartlook.php">カートを見る</a><br />';
-
 }
 catch (Exception $e)
 {
-	 print 'ただいま障害により大変ご迷惑をお掛けしております。';
-	 exit();
+  print 'ただいま障害により大変ご迷惑をお掛けしております。';
+  exit();
 }
-
 ?>
-
 </body>
 </html>
