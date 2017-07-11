e18f7681d75b8920e36df9c06c6cb729f3873168
shimoda4 <shimoda4@users.noreply.github.com>
2017-07-12 06:25:14 +0900

    shimoda-ok

--- sample/common/common.php
+++ sample/common/common.php
@@ -1,7 +1,7 @@
 <?php
 //DEBUG処理の切り替え　　本番運用時にはFALSEに変更すること
-//define('DEBUG',TRUE); //デバッグ時
-define('DEBUG',FALSE);//本番運用時
+define('DEBUG',TRUE);//デバッグ時
+//define('DEBUG',FALSE);//本番運用時        
 ?>
 <?php
 
